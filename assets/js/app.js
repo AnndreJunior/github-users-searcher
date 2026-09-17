91586058524fd6a86a5ea6559c000ffc0c48cc5a
AnndreJunior <anndre.junior15@gmail.com>
2026-09-17 15:48:55 -0300

    Adicionar elemento de notificação na tela quando ocorre algum erro

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,6 +3,35 @@ const GITHUB_API = "https://api.github.com";
 const searchUserForm = document.querySelector("#search-user-form");
 const usernameInput = document.querySelector("#username");
 
+const notificationBox = document.getElementById("notification-box");
+
+function clearNotifications() {
+  notificationBox.innerHTML = "";
+}
+
+function addErrorNotification(error) {
+  const notification = document.createElement("div");
+  notification.classList.add("notification");
+  notification.classList.add("error-notification");
+  notification.setAttribute("role", "alert");
+
+  const closeNotificationBtn = document.createElement("span");
+  closeNotificationBtn.classList.add("close-notificaton-btn");
+  closeNotificationBtn.setAttribute("role", "button");
+  closeNotificationBtn.setAttribute("aria-label", "Fechar");
+
+  const closeNotificationIcon = document.createElement("i");
+  closeNotificationIcon.classList.add("bi");
+  closeNotificationIcon.classList.add("bi-x-lg");
+
+  closeNotificationBtn.appendChild(closeNotificationIcon);
+
+  notification.innerText = error;
+  notification.appendChild(closeNotificationBtn);
+
+  notificationBox.appendChild(notification);
+}
+
 async function searchUser(username) {
   const response = await fetch(`${GITHUB_API}/users/${username}`);
   if (response.status === 404)
@@ -27,8 +56,10 @@ async function searchUser(username) {
   };
 }
 
+// Buscar perfil do github
 searchUserForm.addEventListener("submit", async (event) => {
   event.preventDefault();
+  clearNotifications();
 
   const username = usernameInput.value;
   if (!username) alert("Preencha o input");
@@ -74,6 +105,18 @@ searchUserForm.addEventListener("submit", async (event) => {
     userInfoArticle.innerHTML = "";
     userInfoArticle.innerHTML = userInfoHtml;
   } catch (error) {
-    alert(error.message);
+    addErrorNotification(error.message);
+  }
+});
+
+// Fechar notificações
+notificationBox.addEventListener("click", (event) => {
+  const closeBtn = event.target.closest(".close-notification-btn");
+
+  if (closeBtn) {
+    const notification = closeBtn.closest(".notification");
+    if (notification) {
+      notification.remove();
+    }
   }
 });
